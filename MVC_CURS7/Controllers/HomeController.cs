8def1317ab7e8ecfc303ff683dc587660c53133d
GabrielNegru0 <negru_gabi_94@yahoo.com>
2017-01-25 12:02:52 +0200

    commit

---
+++ MVC_CURS7/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_CURS7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("about")]
+        public ActionResult SomeMethodName()
+        {
+
+            return View();
+
+        }
+        [NonAction]
+        public int Calc()
+        {
+
+
+            return 20;
+        }
+
+        public int DisplayCalc()
+        {
+            return Calc();
+        }
+
+
+        public ActionResult VerbsTest()
+        {
+            return Content("Hello");
+        }
+
+        public ActionResult Verbs()
+        {
+            return Content("Hello");
+        }
+    }
+}
---
+++ MVC_CURS7/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_CURS7.MvcApplication" Language="C#" %>
